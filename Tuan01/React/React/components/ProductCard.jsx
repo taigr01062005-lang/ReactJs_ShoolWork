ed0a9d50c1e7286cd590e7fff47d5f2ee4992524
BaoChan <baophan2929@gmail.com>
2026-01-21 13:04:46 +0700

    Bài 1: Product Card

--- Tuan01/React/React/components/ProductCard.jsx
+++ Tuan01/React/React/components/ProductCard.jsx
@@ -8,7 +8,7 @@ export const ProductCard = () => {
         <img className='productImage' src="https://cdn2.cellphones.com.vn/x/media/catalog/product/i/p/iphone_17_256gb-3_2.jpg?_gl=1*1dvnuat*_gcl_aw*R0NMLjE3Njg3ODY5ODUuRUFJYUlRb2JDaE1JMnR2TV9ieVdrZ01WTVBVOEFoM3g0dzF2RUFBWUFTQUFFZ0x3bl9EX0J3RQ..*_gcl_au*MjAyOTcxMjcxMy4xNzY4Nzg2OTg1*_ga*MTU5NTg2MDIyOC4xNzY4Nzg2OTg1*_ga_QLK8WFHNK9*czE3Njg3ODY5ODUkbzEkZzEkdDE3Njg3ODcwMTYkajI5JGwwJGg5NTcxODk2OTA." alt=""/>
         <h2>Ten San Pham</h2>
         <p>100000</p>
-        <Button/>
+        <Button></Button>
 
     </div>
     
