f105ce2a2469d9d8ddf48e58cbeafb13a771f638
BaoChan <baophan2929@gmail.com>
2026-02-04 17:08:00 +0700

    Hook Review: useMemo

--- Review/HookReview/src/App.jsx
+++ Review/HookReview/src/App.jsx
@@ -5,21 +5,23 @@ import './App.css'
 import { Button } from '../components/Button'
 import { useEffect } from 'react'
 import { useReducer } from 'react'
+import { useMemo } from 'react'
 
-const initialState = {count: 0};
-function reducer(state, action) {
-  switch (action.type) {
-    case 'increment': return { count: state.count + 1 };
-    case 'decrement': return { count: state.count - 1 }; default: throw new Error();
-  }
-}
+
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [count, setCount] = useState(0);
+  const [numbers] = useState([10, 20, 30, 40, 50]);
+
+  const total = useMemo(() => {
+    console.log("Đang tính tổng");
+    return numbers.reduce((acc, cur) => acc + cur, 0);
+  }, [numbers]); // Chỉ tính lại nếu 'numbers' thay đổi
+
+
   return (
     <div>
-      <span>{state.count}</span>
-      <Button type='primary' text='+' onClick={() => { dispatch({ type: 'increment' }) }}></Button>
-      <Button type='danger' text='-' onClick={() => { dispatch({ type: 'decrement' }) }}></Button>
+      <span>{total}</span>
+      <Button type='primary' text='+' onClick={() => { setCount(count + 1) }}></Button>
 
     </div>
 
