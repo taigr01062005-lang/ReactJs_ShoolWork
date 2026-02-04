832827d7f9603163828d972d16bd9b7e356202ea
BaoChan <baophan2929@gmail.com>
2026-02-04 16:46:33 +0700

    Hook Review: useReducer

--- Review/HookReview/src/App.jsx
+++ Review/HookReview/src/App.jsx
@@ -4,21 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from '../components/Button'
 import { useEffect } from 'react'
+import { useReducer } from 'react'
 
+const initialState = {count: 0};
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment': return { count: state.count + 1 };
+    case 'decrement': return { count: state.count - 1 }; default: throw new Error();
+  }
+}
 function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    alert("Hiển thị mỗi khi bấm nút!")
-  });
-
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div>
-        <Button type='primary' text='+' onClick={()=>{setCount(count+1)}}></Button>
-        <span>{count}</span>
+      <span>{state.count}</span>
+      <Button type='primary' text='+' onClick={() => { dispatch({ type: 'increment' }) }}></Button>
+      <Button type='danger' text='-' onClick={() => { dispatch({ type: 'decrement' }) }}></Button>
+
     </div>
-  
+
   )
 }
-
 export default App
