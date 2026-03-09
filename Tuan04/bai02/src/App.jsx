ee9e26edfcf7458aacb418fcf003af4a67f2143f
H82M19\Student <Student>
2026-03-09 08:22:33 +0700

    Bai 2 Tuan 4

--- Tuan04/bai02/src/App.jsx
+++ Tuan04/bai02/src/App.jsx
@@ -2,32 +2,67 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  let [isLoading,setLoading]= useState(true)
+  let [Error,setError] = useState(null)
+
+
+
+  let url='https://jsonplaceholder.typicode.com/users';
+
+  useEffect(() => {
+      // fetch('https://jsonplaceholder.typicode.com/users')
+      //   .then((res) => { 
+      //      return res.json(); })
+      //   .then((datafetch) => {
+      //      setData(datafetch);
+      //      console.log(datafetch);
+      //      return datafetch;
+      //   })
+  
+      async function fetchData() {
+        try {
+          let data=await fetch(url);
+          let res=await data.json();
+          console.log(res)
+         
+          setTimeout(()=>{
+            setData(res)
+            setLoading(false)
+          },3000)
+        } catch (err) {
+          setError(err.message)
+        } 
+      }
+      fetchData()
+     
+  
+  
+    }, [])
+    function isError() {
+        if(Error!=null){
+          alert(Error)
+          }
+        }
+      isError()
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {
+      isLoading?
+      (<p>Loading...</p>):
+      (data.map((item) => {
+        return <div key={item.id}>
+          <h2>{item.name}</h2>
+          <h2>{item.email}</h2>
+        </div>
+
+      }))
+    }
+      
     </>
   )
 }
