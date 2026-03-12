3bb716c5cba0fb85b55a31d4a637cea1c46af2af
BaoChan <baophan2929@gmail.com>
2026-03-12 23:21:01 +0700

    Bài 3 Tuần 4

--- Tuan04/bai03/src/App.jsx
+++ Tuan04/bai03/src/App.jsx
@@ -1,52 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useEffect } from 'react'
 
 function App() {
   const [data, setData] = useState([])
-  let [id,setId]=useState('')
-  let [Error,setError]=useState('')
-  let url=`https://jsonplaceholder.typicode.com/users/${id}`;
-
+  let [id, setId] = useState('')
+  let [Error, setError] = useState(null)
+ 
 
 
   useEffect(() => {
     async function fetchData() {
-      try {
-        let data=await fetch(url);
-        let res=await data.json();
-        console.log(res)
-        setData(res)
-        console.log(id)
-        
-      } catch (error) {
-        setError(error.message)
-       
-      }
-     
-
-    } 
-    fetchData()
-
-
+        let url = `https://jsonplaceholder.typicode.com/users/${id}`;
+        let data = await fetch(url);
+        let res = await data.json();
+        if (!res.name) {
+          setError('Not Found')
+          setData({})
+        } else {
+          setData(res)
+          setError(null)
+        }
+    }
+    if (id) {
+      fetchData()
+    }
   }, [id])
-   console.log(Error)
+
+
+  let checkValid = (e) => {
+    setId(e.target.value)
+  }
 
 
 
   return (
     <>
-    <div>
-      <input onChange={(e)=>{setId(e.target.value)}} type="text"  />
-    </div>
-    {
-      id?(
-      <h2>{data.name} <br />{data.phone} <br /> {data.website}</h2>
-      ):(<h2></h2>)
-    }
-      
+      <div>
+        <input onChange={(e) => { checkValid(e) }} type="text" />
+      </div>
+      {
+        Error ? 
+        (<h2>{Error}</h2>) :
+        (<h2>{data.name} <br />{data.phone} <br /> {data.website}</h2>)
+      }
+
     </>
   )
 }
